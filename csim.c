b382d21b10ae7cf1bbc268eeb5c97f4b2c55bed8
esinonal <46625198+esinonal@users.noreply.github.com>
2020-04-13 01:15:49 +0200

    Add files via upload

---
+++ csim.c
@@ -0,0 +1,200 @@
+#include "cachelab.h"
+#include "unistd.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+//load= Reads
+//M=  read, then write
+//store=  write
+
+//Step0: make our parameters for hit, miss, evictions so we can increment them along way.
+int hits=0;
+int misses=0;
+int evictions=0;
+int globalClock=0;
+
+
+typedef struct theline {
+  int valid;
+  unsigned long long tag;
+  int time;
+}line;
+
+typedef struct theset {
+  line * lines;
+}set;
+
+
+
+void accessCache(unsigned long long tagnum, long setnum, int numLines, set* cache, int s){
+
+  set *currentSet;
+  line *currentLine;
+
+  int tagmatch;
+  int valid;
+  int found;
+  int currentMin;
+  //Go to the set.
+  tagmatch=0;
+  valid=0;
+
+  currentSet= &cache[setnum];
+  currentLine= &((*currentSet).lines[0]);
+
+
+  for (int i=0; i<numLines; i++){ //Go through all the lines in the set.
+    if ((*currentSet).lines[i].tag == tagnum){
+      tagmatch=1;
+      if ((*currentSet).lines[i].valid == 1){
+        valid=1;
+        currentLine= &((*currentSet).lines[i]);
+      }
+    }
+  }
+
+  if ((tagmatch==1)&&(valid==1)){ //Case 1: it is found and it is valid.
+    hits= hits+1; //Increment hit.
+    (*currentLine).time= globalClock; //Say when we accessed the line.
+  }
+  else {
+
+    found=0;
+    for(int i=0; i<numLines; i++){
+      if((*currentSet).lines[i].valid == 0){
+        currentLine= &((*currentSet).lines[i]);
+        found=1;
+      }
+    }
+    if (found==1){ //Case2: there is one with valid bit=0 that we can use.
+      misses= misses+1;
+      (*currentLine).valid=1;
+      (*currentLine).tag=tagnum;
+      (*currentLine).time=globalClock;
+
+    }
+    else if (found==0){ //Case3: we have to evict.
+      misses= misses+1;
+      evictions= evictions+1;
+
+      //Get line to evict:
+      currentMin= (*currentSet).lines[0].time;
+      for(int i=0; i<numLines; i++){
+        if ((*currentSet).lines[i].time < currentMin){
+          currentMin= (*currentSet).lines[i].time;
+        }
+      }
+      for(int i=0; i<numLines; i++){
+        if((*currentSet).lines[i].time==currentMin){
+          currentLine= &((*currentSet).lines[i]);
+        }
+      }
+      (*currentLine).tag=tagnum;
+      (*currentLine).time=globalClock;
+    }
+  }
+
+}
+
+
+void dealWithInstruction(char* string, set* cache, int s, int e, int b){
+  if (string[0]!= 'I'){
+    printf("\n");
+    //Step0: read items from string
+    char mode;
+    unsigned long long address;
+    int d;
+    sscanf(string, " %c %10llx,%d", &mode, &address, &d);
+
+    int t = 64-s-b;
+
+    unsigned long long mask= (unsigned long long)(pow(2,t));
+
+    mask= mask-1;
+    unsigned long long tagnum= ((address>>b)>>s) & mask;
+
+
+    long tagAndSetMask= (-1 << b);
+    long splusb= s+b;
+    long setAndBMask= (pow(2,splusb)-1);
+    long setMaskInMiddle= tagAndSetMask&setAndBMask;
+    long pickupaddress= setMaskInMiddle&address;
+    long setnum= pickupaddress>>b;
+
+    int numLines= e;
+
+
+    //Step1: deal with the item
+    if ((mode == 'L')||(mode == 'S')){ //LOAD/STORE_____________________________
+
+      accessCache(tagnum, setnum, numLines, cache, s);
+
+    }
+    if (mode == 'M'){ //MODIFY_________________________________________________
+      accessCache(tagnum, setnum, numLines, cache, s);
+      accessCache(tagnum, setnum, numLines, cache, s);
+    }
+
+
+  }
+}
+
+int main(int argc, char *argv[]){
+
+
+  //Step1: Read the given arguments
+  int s;
+  int e;
+  int b;
+  char *filename;
+  if (argc==9){
+    s= atoi(argv[2]); //put to int
+    e= atoi(argv[4]);
+    b= atoi(argv[6]);
+    filename= argv[8];
+  }
+  else { //argc==10
+    s= atoi(argv[3]);
+    e= atoi(argv[5]);
+    b= atoi(argv[7]);
+    filename= argv[9];
+  }
+
+  //Step2: Make cache
+  int bigS= pow(2,s);
+
+  set* cache=(set*)malloc(sizeof(set)*bigS); //First, initialize the cache. Cache is an array of sets.
+  for(int i=0;i<bigS;i++) {
+
+    cache[i].lines = (line*) malloc(sizeof(line) *e); //Set is array of lines.
+    for (int j=0; j<e; j++){
+      cache[i].lines[j].tag = 0;
+      cache[i].lines[j].time = 0;
+      cache[i].lines[j].valid = 0;
+
+    }
+  }
+
+
+  //Step3: Read the file and put read it into cache line by line.
+  FILE *fptr;
+  if ((fptr = fopen(filename, "r")) == NULL){
+        printf("Error! opening file");
+        // Program exits if file pointer returns NULL.
+        exit(1);
+  }
+
+  char string[100];
+  while (fgets(string, sizeof(string), fptr) != 0){
+
+    dealWithInstruction(string, cache, s, e, b);
+    globalClock++;
+  }
+  
+  printSummary(hits, misses, evictions);
+
+
+
+  return 0;
+}
---
+++ trans.c
@@ -0,0 +1,195 @@
+/*
+ * trans.c - Matrix transpose B = A^T
+ *
+ * Each transpose function must have a prototype of the form:
+ * void trans(int M, int N, int A[N][M], int B[M][N]);
+ *
+ * A transpose function is evaluated by counting the number of misses
+ * on a 1KB direct mapped cache with a block size of 32 bytes.
+ */
+#include <stdio.h>
+#include "cachelab.h"
+
+int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+void func1(int M, int N, int A[N][M], int B[M][N]);
+void func2(int M, int N, int A[N][M], int B[M][N]);
+void func3(int M, int N, int A[N][M], int B[M][N]);
+/*
+ * transpose_submit - This is the solution transpose function that you
+ *     will be graded on for Part B of the assignment. Do not change
+ *     the description string "Transpose submission", as the driver
+ *     searches for that string to identify the transpose function to
+ *     be graded.
+ */
+char transpose_submit_desc[] = "Transpose submission";
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]){
+        if ((M==32)&&(N==32)){
+          func1(M, N, A, B);
+        }
+        else if ((M==32)&&(N==64)){
+          func2(M, N, A, B);
+       }
+
+        else if ((M==64)&&(N==64)){
+          func3(M, N, A, B);
+        }
+
+}
+
+
+
+
+
+
+
+/*
+ * You can define additional transpose functions below. We've defined
+ * a simple one below to help you get started.
+ */
+
+/*
+ * trans - A simple baseline transpose function, not optimized for the cache.
+ */
+
+char trans_desc[] = "Simple row-wise scan transpose";
+void trans(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tmp;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }
+
+}
+char func1char[] = "func1-32x32";
+void func1(int M, int N, int A[N][M], int B[M][N]){
+
+    int var1;
+    int var2;
+    int var3;
+    int var4;
+    int var5;
+    int var6;
+    int var7;
+    int var8;
+
+    for (int i = 0; i < N; i += 8) {  //block by 8
+      for (int jj = 0; jj < M; jj += 8) {
+        for (int ii = i; ii< i+8; ii++ ){
+
+          var1= A[ii][jj];
+          var2= A[ii][jj+1];
+          var3= A[ii][jj+2];
+          var4= A[ii][jj+3];
+          var5= A[ii][jj+4];
+          var6= A[ii][jj+5];
+          var7= A[ii][jj+6];
+          var8= A[ii][jj+7];
+
+          B[jj+7][ii]=var8;
+          B[jj+6][ii]=var7;
+          B[jj+5][ii]=var6;
+          B[jj+4][ii]=var5;
+          B[jj+3][ii]=var4;
+          B[jj+2][ii]=var3;
+          B[jj+1][ii]=var2;
+          B[jj][ii]=var1;
+        }
+      }
+    }
+}
+
+char func2char[] = "func2-32x64";
+void func2(int M, int N, int A[N][M], int B[M][N]){
+
+    int var1;
+    int var2;
+    int var3;
+    int var4;
+
+    for (int i = 0; i < N; i += 8) {  //block by 4x8
+      for (int jj = 0; jj < M; jj += 4) {
+        for (int ii = i; ii< i+8; ii++ ){
+
+
+          var1= A[ii][jj];
+          var2= A[ii][jj+1];
+          var3= A[ii][jj+2];
+          var4= A[ii][jj+3];
+
+          B[jj+3][ii]=var4;
+          B[jj+2][ii]=var3;
+          B[jj+1][ii]=var2;
+          B[jj][ii]=var1;
+        }
+      }
+    }
+}
+char func3char[] = "func3-64x64";
+void func3(int M, int N, int A[N][M], int B[M][N]){
+
+    int var1;
+    int var2;
+    int var3;
+    int var4;
+
+    for (int i = 0; i < N; i += 4) {  //block by 4x4
+      for (int jj = 0; jj < M; jj += 4) {
+        for (int ii = i; ii< i+4; ii++){
+
+          var1= A[ii][jj];
+          var2= A[ii][jj+1];
+          var3= A[ii][jj+2];
+          var4= A[ii][jj+3];
+
+          B[jj+3][ii]=var4;
+          B[jj+2][ii]=var3;
+          B[jj+1][ii]=var2;
+          B[jj][ii]=var1;
+        }
+      }
+    }
+}
+
+
+/*
+ * registerFunctions - This function registers your transpose
+ *     functions with the driver.  At runtime, the driver will
+ *     evaluate each of the registered functions and summarize their
+ *     performance. This is a handy way to experiment with different
+ *     transpose strategies.
+ */
+void registerFunctions()
+{
+    /* Register any additional transpose functions */
+
+    registerTransFunction(func1, func1char);
+    registerTransFunction(func2, func2char);
+    registerTransFunction(trans, trans_desc);
+    registerTransFunction(func3, func3char);
+    registerTransFunction(transpose_submit,transpose_submit_desc);
+
+}
+
+
+/*
+ * is_transpose - This helper function checks if B is the transpose of
+ *     A. You can check the correctness of your transpose by calling
+ *     it before returning from the transpose function.
+ */
+int is_transpose(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; ++j) {
+            if (A[i][j] != B[j][i]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
